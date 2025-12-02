91a21ea07f7f22b9c64e5549c74f13e5c7bbe734
FranDev99 <francisco.m.ocampo99@gmail.com>
2025-12-01 19:52:32 -0500

    Dependencias de diseño tailwind/shadcn

--- heroes/src/main.tsx
+++ heroes/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Heroes } from "./Heroes";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Heroes />
+  </StrictMode>
+);
