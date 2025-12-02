75571b1b69a461d67c2fe0d9a3cdd020c6a8f6e8
FranDev99 <francisco.m.ocampo99@gmail.com>
2025-12-01 21:08:44 -0500

    Creación de las rutas con ReactRouter

--- heroes/src/main.tsx
+++ heroes/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import { HeroesApp } from "./HeroesApp";
+
 import "./index.css";
-import { Heroes } from "./Heroes";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Heroes />
+    <HeroesApp />
   </StrictMode>
 );
