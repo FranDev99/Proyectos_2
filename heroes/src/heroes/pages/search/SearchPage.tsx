d7ffcd8f966347fa04eb8f9f5dca37be5af2c557
FranDev99 <francisco.m.ocampo99@gmail.com>
2025-12-02 21:27:48 -0500

    Maquetación componentes iniciales HeroStats/SearchControls

--- heroes/src/heroes/pages/search/SearchPage.tsx
+++ heroes/src/heroes/pages/search/SearchPage.tsx
@@ -1,12 +1,21 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron";
+import { HeroStats } from "@/heroes/components/HeroStats";
+import { SearchControls } from "./ui/SearchControls";
 
 export const SearchPage = () => {
   return (
     <>
+      {/* Header */}
       <CustomJumbotron
         tittle="Búsqueda de SuperHéroes"
         description="Descubre, explora y administra tus héroes y villanos favoritos"
       />
+
+      {/* Stats Dashboard */}
+      <HeroStats />
+
+      {/* Controls */}
+      <SearchControls />
     </>
   );
 };
