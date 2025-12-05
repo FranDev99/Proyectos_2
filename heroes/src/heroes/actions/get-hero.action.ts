db4b2d35a0f05a0ba18817c552d95de4d6edf382
FranDev99 <francisco.m.ocampo99@gmail.com>
2025-12-04 21:39:15 -0500

    Página con la información detallada de cada personaje

---
+++ heroes/src/heroes/actions/get-hero.action.ts
@@ -0,0 +1,14 @@
+import type { Hero } from "@/types/hero.interface";
+import { heroApi } from "../api/hero.api";
+
+
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+export const getHeroAction = async (idSlug: string) => {
+    const { data } = await heroApi.get<Hero>(`/${idSlug}`)
+
+    return {
+        ...data,
+        image: `${BASE_URL}/images/${data.image}`
+    }
+}
