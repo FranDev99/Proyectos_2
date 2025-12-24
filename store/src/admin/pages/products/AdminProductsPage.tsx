c37a024ca3dee13a8019e0e71545ab603839900b
FranDev99 <francisco.m.ocampo99@gmail.com>
2025-12-23 23:03:24 -0500

    Maquetación de pagina para gestion de productos

--- store/src/admin/pages/products/AdminProductsPage.tsx
+++ store/src/admin/pages/products/AdminProductsPage.tsx
@@ -1,3 +1,71 @@
+import { AdminTitle } from "@/admin/components/AdminTitle";
+import { CustomPagination } from "@/components/custom/CustomPagination";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { PlusIcon } from "lucide-react";
+import { Link } from "react-router";
+
 export const AdminProductsPage = () => {
-  return <div>AdminProductsPage</div>;
+  return (
+    <>
+      <div className="flex justify-between items-center">
+        <AdminTitle
+          title="Productos"
+          subTitle="Aquí puedes ver y administrar tus productos"
+        />
+        <div className="flex justify-end mb-10 gap-4">
+          <Link to={"/admin/products/new"}>
+            <Button>
+              <PlusIcon />
+              Nuevo Producto
+            </Button>
+          </Link>
+        </div>
+      </div>
+
+      <Table className="bg-white p-10 shadow-xs border border-gray-200 mb-10">
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-25">ID</TableHead>
+            <TableHead>Imagen</TableHead>
+            <TableHead>Nombre</TableHead>
+            <TableHead>Precio</TableHead>
+            <TableHead>Categoría</TableHead>
+            <TableHead>Cantidad</TableHead>
+            <TableHead>Tallas</TableHead>
+            <TableHead className="text-right">Acciones</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">INV001</TableCell>
+            <TableCell>
+              <img
+                src=""
+                alt=""
+                className="w-20 h-20 object-cover rounded-md"
+              />
+            </TableCell>
+            <TableCell>Camiseta</TableCell>
+            <TableCell>$250</TableCell>
+            <TableCell>Categoría 1</TableCell>
+            <TableCell>25</TableCell>
+            <TableCell>S,M,L</TableCell>
+            <TableCell className="text-right">
+              <Link to={`/admin/products/t-shirt-teslo`}>Editar</Link>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+
+      <CustomPagination totalPages={5} />
+    </>
+  );
 };
