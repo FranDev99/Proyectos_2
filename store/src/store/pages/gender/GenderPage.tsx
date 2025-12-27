fa10f557d94c193634221b64749b6a5508235eb7
FranDev99 <francisco.m.ocampo99@gmail.com>
2025-12-27 17:23:14 -0500

    Datos dinámicos desde el backend de la aplicación

--- store/src/store/pages/gender/GenderPage.tsx
+++ store/src/store/pages/gender/GenderPage.tsx
@@ -1,10 +1,11 @@
 import { CustomPagination } from "@/components/custom/CustomPagination";
-import { products } from "@/mocks/products.mocks";
 import { CustomJumbotron } from "@/store/components/CustomJumbotron";
 import { ProductsGrid } from "@/store/components/ProductsGrid";
+import { useProducts } from "@/store/hooks/useProducts";
 import { useParams } from "react-router";
 
 export const GenderPage = () => {
+  const { data } = useProducts();
   const { gender } = useParams();
   const genderLabel =
     gender === "men" ? "Hombres" : gender === "women" ? "Mujeres" : "Niños";
@@ -12,7 +13,7 @@ export const GenderPage = () => {
   return (
     <>
       <CustomJumbotron tittle={`Productos para ${genderLabel}`} subTittle="" />
-      <ProductsGrid products={products} />
+      <ProductsGrid products={data?.products || []} />
       <CustomPagination totalPages={5} />
     </>
   );
--- store/src/store/pages/home/HomePage.tsx
+++ store/src/store/pages/home/HomePage.tsx
@@ -1,14 +1,16 @@
 import { CustomPagination } from "@/components/custom/CustomPagination";
-import { products } from "@/mocks/products.mocks";
 import { CustomJumbotron } from "@/store/components/CustomJumbotron";
 import { ProductsGrid } from "@/store/components/ProductsGrid";
+import { useProducts } from "@/store/hooks/useProducts";
 
 export const HomePage = () => {
+  const { data } = useProducts();
+
   return (
     <>
       <CustomJumbotron tittle="Todos los productos" subTittle="" />
-      <ProductsGrid products={products} />
-      <CustomPagination totalPages={5} />
+      <ProductsGrid products={data?.products || []} />
+      <CustomPagination totalPages={data?.pages || 0} />
     </>
   );
 };
