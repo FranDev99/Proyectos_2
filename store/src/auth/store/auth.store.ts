fd8b088a3d8eb497564c6dc07563867a4c4882fb
FranDev99 <francisco.m.ocampo99@gmail.com>
2026-01-03 19:09:34 -0500

    Conexión de tanstack y zustand para manejo de autenticaciones

--- store/src/auth/store/auth.store.ts
+++ store/src/auth/store/auth.store.ts
@@ -1,8 +1,9 @@
 import type { User } from '@/interfaces/user.interface'
 import { create } from 'zustand'
 import { loginAction } from '../actions/login.action'
+import { checkAuthAction } from '../actions/check-auth.action';
 
-type AuthStatus = 'aunthenticated' | 'not-authenticated' | 'checking';
+type AuthStatus = 'authenticated' | 'not-authenticated' | 'checking';
 
 type AuthState = {
     user: User | null
@@ -11,6 +12,7 @@ type AuthState = {
 
     login: (email: string, password: string) => Promise<boolean>
     logout: () => void;
+    checkAuthStatus: () => Promise<boolean>;
 }
 
 export const useAuthStore = create<AuthState>()((set) => ({
@@ -23,11 +25,11 @@ export const useAuthStore = create<AuthState>()((set) => ({
             const data = await loginAction(email, password);
             localStorage.setItem('token', data.token);
 
-            set({ user: data.user, token: data.token });
+            set({ user: data.user, token: data.token, authStatus: "authenticated" });
             return true;
 
         } catch (error) {
-            set({ user: null, token: null });
+            set({ user: null, token: null, authStatus: "not-authenticated" });
             localStorage.removeItem('token');
             return false;
         }
@@ -35,6 +37,26 @@ export const useAuthStore = create<AuthState>()((set) => ({
 
     logout: () => {
         localStorage.removeItem('token')
-        set({ user: null, token: null });
+        set({ user: null, token: null, authStatus: "not-authenticated" });
+    },
+
+    checkAuthStatus: async () => {
+
+        try {
+            const { user, token } = await checkAuthAction();
+            set({
+                user: user,
+                token: token,
+                authStatus: "authenticated"
+            })
+            return true
+        } catch (error) {
+            set({
+                user: undefined,
+                token: undefined,
+                authStatus: "not-authenticated"
+            })
+            return false
+        }
     }
 }))
