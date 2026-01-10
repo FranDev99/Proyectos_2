1b3c9fea323a05410e2f0147f973b6d652bc0dae
FranDev99 <francisco.m.ocampo99@gmail.com>
2026-01-10 16:20:21 -0500

    Acción para obtener producto por ID y validaciones en la URL

--- store/src/admin/pages/product/AdminProductPage.tsx
+++ store/src/admin/pages/product/AdminProductPage.tsx
@@ -1,10 +1,11 @@
 import { AdminTitle } from "@/admin/components/AdminTitle";
-import { useParams } from "react-router";
+import { Navigate, useParams } from "react-router";
 
 import { useState } from "react";
 import { X, Plus, Upload, Tag, SaveAll } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router";
+import { useProduct } from "@/admin/hooks/useProduct";
 
 interface Product {
   id: string;
@@ -21,6 +22,7 @@ interface Product {
 
 export const AdminProductPage = () => {
   const { id } = useParams();
+  const { isLoading, isError, data: product2 } = useProduct(id || "");
 
   const productTitle = id === "new" ? "Nuevo producto" : "Editar producto";
   const productSubtitle =
@@ -51,6 +53,10 @@ export const AdminProductPage = () => {
   const [dragActive, setDragActive] = useState(false);
 
   const availableSizes = ["XXS", "XS", "S", "M", "L", "XL", "XXL", "XXXL"];
+
+  if (isError) {
+    return <Navigate to="/admin/products" />;
+  }
 
   const handleInputChange = (field: keyof Product, value: string | number) => {
     setProduct((prev) => ({ ...prev, [field]: value }));
