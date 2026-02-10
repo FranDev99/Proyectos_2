50424e92edf91e389f7135134c37bf2338d5f5aa
FranDev99 <francisco.m.ocampo99@gmail.com>
2026-02-10 12:19:07 -0500

    Configuración del CORS para las peticiones del front end

--- devtree-backend/src/server.ts
+++ devtree-backend/src/server.ts
@@ -1,14 +1,22 @@
 // const express = require('express') // CJS Common Js
 import express, { Application } from 'express' // ESM ECMAScript modules
+import cors from 'cors'
 import 'dotenv/config'
 import router from './router'
 import { connectDB } from './config/db'
+import { corsConfig } from './config/cors'
+
+//conexión a la base de datos
+connectDB()
 
 const app: Application = express()
 
-connectDB()
+//cors
+app.use(cors(corsConfig))
 
+//Leer datos del formulario
 app.use(express.json())
+
 app.use('/', router)
 
 export default app
