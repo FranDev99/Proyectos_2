eab035c3c745f62dd530d6fb295683bee23cb347
FranDev99 <francisco.m.ocampo99@gmail.com>
2026-02-26 20:47:56 -0500

    Funcionalidad para activar o desactivar URL

--- devtree/src/views/LinkTreeView.tsx
+++ devtree/src/views/LinkTreeView.tsx
@@ -5,8 +5,18 @@ import { DevTreeInput } from "../components/DevTreeInput";
 export const LinkTreeView = () => {
   const [devTreeLinks, setDevTreeLinks] = useState(social);
 
-  const handleUrlChange = () => {
-    return;
+  const handleUrlChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const updatedLinks = devTreeLinks.map((link) =>
+      link.name === e.target.name ? { ...link, url: e.target.value } : link,
+    );
+    setDevTreeLinks(updatedLinks);
+  };
+
+  const handleEnableLink = (socialNetwork: string) => {
+    const updatedLinks = devTreeLinks.map((link) =>
+      link.name === socialNetwork ? { ...link, enabled: !link.enabled } : link,
+    );
+    setDevTreeLinks(updatedLinks);
   };
 
   return (
@@ -17,6 +27,7 @@ export const LinkTreeView = () => {
             key={item.name}
             item={item}
             handleUrlChange={handleUrlChange}
+            handleEnableLink={handleEnableLink}
           />
         ))}
       </div>
