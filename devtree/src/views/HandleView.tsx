8eb4cdfbb4f80adb540822a77c52345415c0fb4c
FranDev99 <francisco.m.ocampo99@gmail.com>
2026-03-05 16:47:05 -0500

    API para la petición de datos según el handle del usuario, para el perfil público

--- devtree/src/views/HandleView.tsx
+++ devtree/src/views/HandleView.tsx
@@ -1,3 +1,15 @@
+import { useParams } from "react-router";
+import { useQuery } from "@tanstack/react-query";
+import { getUserByHandle } from "../api/DevTreeApi";
+
 export const HandleView = () => {
+  const params = useParams();
+  const handle = params.handle!;
+  const { data, error, isLoading } = useQuery({
+    queryFn: () => getUserByHandle(handle),
+    queryKey: ["handle", handle],
+    retry: 1,
+  });
+
   return <div>HandleView</div>;
 };
